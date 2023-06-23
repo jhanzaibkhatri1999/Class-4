7e007a17896de97e9c743479a680480e83737be4
jhanzeb.khatri1561@gmail.com <jhanzeb.khatri1561@gmail.com>
2023-06-23 22:17:05 +0500

    Class 4 added

---
+++ if_else.dart
@@ -0,0 +1,19 @@
+void main() {
+
+num num1 = 10;
+num num2 = 12;
+if (num1 > num2){
+print("num1 is greater than num2 ");
+}
+else if(num1 == num2){
+  print("num1 is equal to num2");
+
+}
+else {
+print("num2 is greater than num1");
+
+
+}
+
+
+}
---
+++ list.dart
@@ -0,0 +1,27 @@
+void main() {
+
+// var name1 = "Bilal";
+// var name2 = "Taber";
+
+List<String> studentNames = ["Bilal","Taber","Hamza"];
+// print(studentNames[1]);
+// print(studentNames.first);
+// studentNames.replaceRange(0, 1, ["why"]);
+// print(studentNames);
+
+// studentNames.sort();
+
+// studentNames.add("abc");
+
+// List abc = List.of(studentNames.reversed);
+// studentNames.insertAll(1, ["abc"]);
+// studentNames.removeWhere((element) => false);
+
+ for (String studentName in studentNames)
+print(studentName);
+
+
+
+
+  
+}
